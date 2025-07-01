8cd4b3a77158a89a3f2e7bef5a5e328d707b69f8
Denis Kiprotich <146120658+denisktoo@users.noreply.github.com>
2025-07-01 15:23:44 -0700

    Subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,20 @@
+-- Find all properties where the average rating is greater than 4.0 using a subquery.
+SELECT 
+    p.*
+FROM 
+    property b
+    JOIN Review r ON p.property_id = r.property_id
+WHERE 
+    r.rating > 4.0;
+
+-- Correlated subquery to find users who have made more than 3 bookings
+SELECT 
+    u.*
+FROM 
+    User u
+WHERE 
+    (
+        SELECT COUNT(*) 
+        FROM Booking b 
+        WHERE b.user_id = u.user_id
+    ) > 3;
