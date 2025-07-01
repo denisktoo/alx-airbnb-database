cd12be9d9b5d9a961f49b2c43ff7dce36f265353
Denis Kiprotich <146120658+denisktoo@users.noreply.github.com>
2025-07-01 15:00:42 -0700

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,31 +1,21 @@
+-- Using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
 SELECT
   b.*,
   u.*
-  -- u.user_id
-  -- u.first_name
-  -- u.last_name
-  -- u.email
-  -- u.password_hash
-  -- u.phone_number
-  -- u.role
-  -- u.created_at
   FROM Booking as b
-  INNER JOIN User u on b.user_id = u.user_id
+  INNER JOIN User u on b.user_id = u.user_id;
 
+-- Using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 SELECT
   p.*,
   r.*
-  -- r.review_id
-  -- r.property_id
-  -- r.user_id
-  -- r.rating
-  -- r.comment
-  -- r.created_at
   FROM Property p
   LEFT JOIN Review r on p.property_id = r.property_id
+  ORDER BY p.property_id, r.created_at DESC;
 
+-- Using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT
   b.*,
   u.*
   FROM User u
-  FULL OUTER JOIN Booking b on b.user_id = u.user_id
+  FULL OUTER JOIN Booking b on b.user_id = u.user_id;
