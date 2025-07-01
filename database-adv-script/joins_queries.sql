5fa98f720d35d481684e3463ecbb05e37721aec7
Denis Kiprotich <146120658+denisktoo@users.noreply.github.com>
2025-07-01 14:46:15 -0700

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,31 @@
+select
+  b.*,
+  u.*
+  -- u.user_id
+  -- u.first_name
+  -- u.last_name
+  -- u.email
+  -- u.password_hash
+  -- u.phone_number
+  -- u.role
+  -- u.created_at
+  from Booking as b
+  inner join User u on b.user_id = u.user_id
+
+select
+  p.*,
+  r.*
+  -- r.review_id
+  -- r.property_id
+  -- r.user_id
+  -- r.rating
+  -- r.comment
+  -- r.created_at
+  from Property p
+  left join Review r on p.property_id = r.property_id
+
+select
+  b.*,
+  u.*
+  from User u
+  full outer join Booking b on b.user_id = u.user_id
