10a04fb8767943e9d4a9a99326755103a20dfc5b
Denis Kiprotich <146120658+denisktoo@users.noreply.github.com>
2025-07-02 08:55:30 -0700

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -24,4 +24,6 @@ SELECT
 FROM Booking b
 INNER JOIN User u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
-INNER JOIN Payment pay ON b.booking_id = pay.booking_id;
+INNER JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND b.created_at >= CURDATE() - INTERVAL 30 DAY;
