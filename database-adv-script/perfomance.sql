69d276038f22f3f22f6f5e28df3b9dad9d4c13c9
Denis Kiprotich <146120658+denisktoo@users.noreply.github.com>
2025-07-02 08:45:02 -0700

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,27 @@
+-- Initial query that retrieves all bookings along with the user details, property details, and payment details
+SELECT
+  b.*,
+  u.*,
+  p.*,
+  pay.*
+FROM Booking b
+INNER JOIN User u on b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+INNER JOIN Payment pay ON b.booking_id = pay.booking_id;
+
+-- Optimized query
+SELECT
+  b.booking_id,
+  b.start_date,
+  b.end_date,
+  b.status,
+  u.first_name,
+  u.last_name,
+  p.name AS property_name,
+  p.location,
+  pay.amount,
+  pay.payment_date
+FROM Booking b
+INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+INNER JOIN Payment pay ON b.booking_id = pay.booking_id;
